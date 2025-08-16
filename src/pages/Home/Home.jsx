45edbb6dc494e163d4fa6b1f93d24469f70b8088
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-16 21:53:24 +0530

    Layout struycture change v2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import FAQ from "./8th";
 import FourthSectiion from "./Fourth";
-import Hero from "./Hero";
-import Second from "./Second";
+
 import Sixth from "./Sixth";
 
 function Home() {
   return (
     <div className="w-full" >
-      <Hero />
-      <Second/>
+      
       <FourthSectiion/>
       <Sixth/>
       <FAQ/>
