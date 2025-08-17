c27cd7f35eeb2b614ce65c7ae4c5e2576f80f8ed
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-17 23:25:48 +0530

    UI updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import Footer from "../../components/Common/Footer";
 import FAQ from "./8th";
 import FourthSectiion from "./Fourth";
+import Hero from "./Hero";
+import Second from "./Second";
 
 import Sixth from "./Sixth";
 
 function Home() {
   return (
     <div  >
-      
+      <Hero />
+      <Second />
       <FourthSectiion/>
       <Sixth/>
       <FAQ/>
