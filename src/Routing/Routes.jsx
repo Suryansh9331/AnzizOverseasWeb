6584e46ad4478c7a56f1879734a68e3b9379b11f
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-18 23:55:35 +0530

    router setup v6

--- src/Routing/Routes.jsx
+++ src/Routing/Routes.jsx
@@ -6,6 +6,8 @@ const Home = lazy(() => import("../pages/Home/Home"));
 
 // Layout components
 import Footer from "../components/Common/Footer";
+import About from "../Pages/About/About";
+import Contact from "../pages/Contact/Contact";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +15,8 @@ const AppRoutes = () => {
       <Suspense fallback={<div className="text-center p-10">Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
           
         </Routes>
       </Suspense>
